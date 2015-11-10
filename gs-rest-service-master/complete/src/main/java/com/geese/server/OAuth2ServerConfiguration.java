def5c502b40b76a5e455ce3f3cb8d80361f4a688
JMtorii <j.torii4@gmail.com>
2015-11-09 20:43:52 -0500

    Checkpoint :)

--- gs-rest-service-master/complete/src/main/java/com/geese/server/OAuth2ServerConfiguration.java
+++ gs-rest-service-master/complete/src/main/java/com/geese/server/OAuth2ServerConfiguration.java
@@ -1,115 +1,122 @@
 package com.geese.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.web.savedrequest.NullRequestCache;
 
 
 /**
+ * If you want to add the resource server, add it to this class. JWT does this on the fly so it's not necessary to
+ * configure it in this implementation.
+ *
  * Created by JMtorii on 2015-10-31.
  */
 @Configuration
+@EnableResourceServer
 public class OAuth2ServerConfiguration {
 
-    @Configuration
-    @EnableResourceServer
-    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-
-        @Autowired
-        private TokenStore tokenStore;
-
-        @Override
-        public void configure(ResourceServerSecurityConfigurer resources) throws Exception{
-            resources.tokenStore(tokenStore);
-        }
-
-        @Override
-        public void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
-            http.authorizeRequests().anyRequest().authenticated();
-//                    .antMatchers("/users").hasRole("ADMIN")
-//                    .antMatchers("/greeting").authenticated();
-            // @formatter:on
-        }
-
+    @Bean
+    public ApplicationSecurity applicationSecurity() {
+        return new ApplicationSecurity();
     }
 
+    @Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
+    @Configuration
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .anonymous()
+                    .disable()
+                    .csrf()
+                    .disable()
+                    .authorizeRequests()
+                    .antMatchers("/api/**").hasRole("USER")
+                    .antMatchers("/management/**").hasRole("ADMIN")
+                    .and()
+                    .requestCache()
+                    .requestCache(new NullRequestCache())
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("admin").password("pwd").roles("ADMIN").and()
+                    .withUser("user").password("pwd").roles("USER");
+        }
+    }
+
+
+    /**
+     * Server issuing access tokens to client after successfully authenticating the resource owner and obtaining
+     * authorization.
+     */
     @Configuration
     @EnableAuthorizationServer
     protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
         @Autowired
-        private AuthenticationManager auth;
-
-        @Autowired
-        private DataSource dataSource;
-
-        private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        private AuthenticationManager authenticationManager;
 
         @Bean
-        public JdbcTokenStore tokenStore() {
-            return new JdbcTokenStore(dataSource);
-        }
-
-        @Bean
-        protected AuthorizationCodeServices authorizationCodeServices() {
-            return new JdbcAuthorizationCodeServices(dataSource);
+        public JwtAccessTokenConverter accessTokenConverter() {
+            return new JwtAccessTokenConverter();
         }
 
         @Override
-        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-            security.passwordEncoder(passwordEncoder);
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            oauthServer.tokenKeyAccess("isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')").checkTokenAccess(
+                    "hasAuthority('ROLE_TRUSTED_CLIENT')");
         }
 
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-            endpoints.authorizationCodeServices(authorizationCodeServices())
-                    .authenticationManager(auth).tokenStore(tokenStore())
-                    .approvalStoreDisabled();
+            endpoints.authenticationManager(authenticationManager).accessTokenConverter(accessTokenConverter());
         }
 
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-            clients.jdbc(dataSource)
-                    .passwordEncoder(passwordEncoder)
+            // @formatter:off
+            clients.inMemory()
                     .withClient("my-trusted-client")
                     .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
                     .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
                     .scopes("read", "write", "trust")
-                    .resourceIds("oauth2-resource")
-                    .accessTokenValiditySeconds(60).and()
+                    .accessTokenValiditySeconds(60)
+                .and()
                     .withClient("my-client-with-registered-redirect")
                     .authorizedGrantTypes("authorization_code")
-                    .authorities("ROLE_CLIENT").scopes("read", "trust")
-                    .resourceIds("oauth2-resource")
-                    .redirectUris("http://anywhere?key=value").and()
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read", "trust")
+                    .redirectUris("http://anywhere?key=value")
+                .and()
                     .withClient("my-client-with-secret")
                     .authorizedGrantTypes("client_credentials", "password")
-                    .authorities("ROLE_CLIENT").scopes("read")
-                    .resourceIds("oauth2-resource").secret("secret");
+                    .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                    .scopes("read", "write")
+                    .secret("secret");
+            // @formatter:on
         }
-
-//        @Autowired
-//        public void init(AuthenticationManagerBuilder auth) throws Exception {
-//            auth.jdbcAuthentication().dataSource(dataSource).withUser("dave").password("secret").roles("USER");
-//        }
 
     }
 
